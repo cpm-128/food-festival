579aae40bd86d515b3a06afa8a2be4c891df79c7
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-18 13:11:52 -0400

    active service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-// the code in stall the service worker by adding files to the prechase so the app can use the cache
-
 const APP_PREFIX = 'FoodFest-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
@@ -19,6 +17,7 @@ const FILES_TO_CACHE = [
     './dist/schedule.bundle.js'
 ];
 
+// the code in stall the service worker by adding files to the prechase so the app can use the cache
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -27,3 +26,23 @@ self.addEventListener('install', function (e) {
         })
     )
 })
+
+// activate the service worker
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('>> Deleting cache >> ' + keyList[i] );
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
